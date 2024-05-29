e080db83b0be506f45e1684f57aa7721ce25b25e
Michael Daley <mgd3@njit.edu>
2024-05-29 03:22:55 -0400

    began working on displaying board and player choices to page

--- js/app.js
+++ js/app.js
@@ -1,45 +1,75 @@
 const gameboard = (() => {
+    const pageBoard = Array.from(document.querySelectorAll(".gameboard .square"));
     const squares = [];
-    const emptySquares = [];
     let numOfSquares = 9;
 
     //creating empty board
 
+
     for(let i = 0; i < numOfSquares; i++){
-        const square = {player:null};
-        emptySquares.push(square);
+        const square = {location:i + 1, player:null};
         squares.push(square);
     }
 
     const occupySquare = (location,player) => {
         if(typeof location !== 'number'){
-            console.log(`occupySquare: location value must be a "number"`)
+            console.log(`occupySquare: location value must be a "number"`);
+            return;
         }
 
-        if(location > 9){
+        if(location < 1 || location > 9){
             console.log(`occupySquare: "${location}" is not a valid square loaction`);
             return;
         }
 
-        let arrayPos = location - 1;
-        let currSquare = squares[arrayPos];
+        if(typeof player !== 'object' || ((!player.id) || (!player.name) || (!player.occupiedSquares))){
+            console.log(`occupySquare: "${JSON.stringify(player)}" is not a valid player object`);
+            return;
+        }
+
+        let currSquare = squares[location - 1];
+        let pageSquare = pageBoard[location - 1]
+
         if(currSquare.player){
             console.log(`occupySquare: square location "${location}" is already occupied`);
             return;
         }
-
-        emptySquares.splice(arrayPos,1);
+ 
         currSquare.player = player;
+        player.occupiedSquares.push(location);
+        pageSquare.style.backgroundImage = "url(../images/X.png)";
+
     }
+
+
+    let playerOne = {id: 1, name: 'player one', occupiedSquares:[]}
+    pageBoard.forEach(element => {
+        element.addEventListener("click", () => {
+            gameboard.occupySquare(pageBoard.indexOf(element) + 1, playerOne);
+        });
+    });
 
     const displaySquares = () =>{
         console.log(squares);
-        console.log(emptySquares);
+        console.log(pageBoard);
     }
 
     return{occupySquare, displaySquares};
 
 })();
+
+const game = (() => {
+    const pageBoard = Array.from(document.querySelectorAll(".gameboard .square"));
+
+    // let keys = Object.keys(currPlayers);
+    // personTurn = currPlayers[keys.length * Math.random() << 0];
+    
+
+
+
+})();
+
+gameboard.displaySquares();
 
 // const bot = (() => {
 //      let randSquarePos = (gameboard.avaliableSquares().length * Math.random()) << 0;
